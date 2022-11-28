cd7cedb2e4e3986eeaebf0e0f0b5b1389c99eb7e
Misiki77 <119290880+Misiki77@users.noreply.github.com>
2022-11-28 15:18:55 +0200

    my first lab

---
+++ Source.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    float x, y, S;
+
+    printf("Write the numbers:");
+    scanf("%f", &x);
+    scanf("%f", &y);
+    printf("x = %f, y = %f \n", x, y);
+
+    S = 3.6*pow(sin(x) + cos(pow(y,2)), 3) - tan(sqrt(pow(sin(x), 2) + pow(cos(x), 2)));
+
+    printf("Your answer is: %f", S);
+
+    return 0;
+
+}
